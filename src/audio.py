c24ac8e4e107362999541c3f13d2ef5e6108308b
Andy Vallejos Bascope <AndyVallejosGB@gmail.com>
2026-05-06 00:37:07 -0400

    fix:resolve conflicts

--- src/audio.py
+++ src/audio.py
@@ -7,7 +7,6 @@ import soundfile as sf
 
 
 def talk(text):
-<<<<<<< desarrollo-Gramatica
     hablar = pyttsx3.init()
     voz = hablar.getProperty('voices')
     hablar.setProperty('voice', voz[0].id)
@@ -44,40 +43,3 @@ def run():
 
 if __name__ == "__main__":
     run()
-=======
-    hablar.say(text)
-    hablar.runAndWait() 
-
-    hablar=pyttsx3.init()
-    voz=hablar.getProperty('voices')
-    hablar.setProperty('voice', voz[0].id)
- 
-
-
-def grabar(nombre_archivo, tiempo):
-    frec=44100
-    canales=1
-
-    print("grabando...")
-    grabacion=sd.rec(int(tiempo*frec),samplerate=frec,channels=canales,dtype='int16')
-    sd.wait()
-    print("grabacion terminada")
-    wab=nombre_archivo + ".wav"
-    sf.write(wab, grabacion, frec)
-    audio_segment=AudioSegment.from_wav(wab)
-    arc_mp3=nombre_archivo+".mp3"
-    audio_segment.export(arc_mp3, format="mp3")
-    os.remove(wab)
-
-def run():
-    talk("que titulo tendra el audio?")
-    titulo=input("ingresa el título")
-    talk("que tiempo durara la grabacion?")
-    tiempo=int(input("ingresa el tiempo"))
-    talk("puedes comenzar a hablar")
-    grabar(titulo,tiempo)
-    talk("la grabacion ha terminado")
-
-if __name__ == "__main__":
-    run()
->>>>>>> main
