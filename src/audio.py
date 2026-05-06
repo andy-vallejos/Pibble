a14bd58f64065a078765837000a087b19cb6a2ba
Andy Vallejos <andyvallejosgb@gmail.com>
2026-05-06 00:16:19 -0400

    fix: order

--- src/audio.py
+++ src/audio.py
@@ -7,38 +7,39 @@ import soundfile as sf
 
 
 def talk(text):
-    hablar.say(text)
-    hablar.runAndWait() 
-
-    hablar=pyttsx3.init()
-    voz=hablar.getProperty('voices')
+    hablar = pyttsx3.init()
+    voz = hablar.getProperty('voices')
     hablar.setProperty('voice', voz[0].id)
- 
+    hablar.say(text)
+    hablar.runAndWait()
 
 
 def grabar(nombre_archivo, tiempo):
-    frec=44100
-    canales=1
+    frec = 44100
+    canales = 1
 
     print("grabando...")
-    grabacion=sd.rec(int(tiempo*frec),samplerate=frec,channels=canales,dtype='int16')
+    grabacion = sd.rec(int(tiempo*frec), samplerate=frec,
+                       channels=canales, dtype='int16')
     sd.wait()
     print("grabacion terminada")
-    wab=nombre_archivo + ".wav"
+    wab = nombre_archivo + ".wav"
     sf.write(wab, grabacion, frec)
-    audio_segment=AudioSegment.from_wav(wab)
-    arc_mp3=nombre_archivo+".mp3"
+    audio_segment = AudioSegment.from_wav(wab)
+    arc_mp3 = nombre_archivo+".mp3"
     audio_segment.export(arc_mp3, format="mp3")
     os.remove(wab)
 
+
 def run():
     talk("que titulo tendra el audio?")
-    titulo=input("ingresa el título")
+    titulo = input("ingresa el título")
     talk("que tiempo durara la grabacion?")
-    tiempo=int(input("ingresa el tiempo"))
+    tiempo = int(input("ingresa el tiempo"))
     talk("puedes comenzar a hablar")
-    grabar(titulo,tiempo)
+    grabar(titulo, tiempo)
     talk("la grabacion ha terminado")
+
 
 if __name__ == "__main__":
     run()
